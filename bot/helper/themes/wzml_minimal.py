4cfdf40c9651a2b31f3d1cba6d880f466d895093
TKMO TY-TK <heroherokucc@gmail.com>
2024-08-13 06:52:22 +0000

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -3,7 +3,7 @@ class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
     ST_BN1_NAME = 'Meh Upaates ❤️'
-    ST_BN1_URL = ''https://t.me/TK_MOB_TY_1'
+    ST_BN1_URL = 'https://t.me/TK_MOB_TY_1'
     ST_BN2_NAME = 'Creator 🧑‍🔧'
     ST_BN2_URL = 'https://t.me/TK_Mob_TY'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
